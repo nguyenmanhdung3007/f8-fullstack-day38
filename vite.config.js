01d013e17db5249cca774e82f4fa0739528edbec
nguyenmanhdung3007 <nguyenmanhdung3007@gmail.com>
2025-12-03 19:57:45 +0100

    sua lai bai cho

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/f8-fullstack-day38",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
